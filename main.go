018ad38774c40bcd372c20d6a4a5ab39c9957196
Compico <compico@toby3d.ru>
2019-07-12 00:01:56 +0300

    some code

--- main.go
+++ main.go
@@ -2,25 +2,34 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"path/filepath"
+	"strconv"
 	"strings"
 
 	"golang.org/x/sys/windows/registry"
 )
 
-//OsuPath path to osu
-type OsuPath struct {
-	SongsPath string
-	Err       error
+//SongFolder {id-author-name}
+type SongFolder struct {
+	ID     []string
+	Artist string
+	Title  string
 }
 
 func main() {
-	var osuPath OsuPath
-	osuPath.SongsPath, osuPath.Err = osuSongsPath()
-	if osuPath.Err != nil {
+	songsPath, err := osuSongsPath()
+	if err != nil {
 		fmt.Printf("osu! not installed")
 	}
-
+	songsFolders, err := getID(songsPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, songFolder := range songsFolders {
+		fmt.Println(songFolder)
+	}
 }
 
 // return "audio.mp3"
@@ -28,9 +37,25 @@ func findMp3File(songPath string) (string, error) {
 	return "", nil
 }
 
-// return "id artist - songname"
-func pathSong(songsPath string) (string, error) {
-	return "", nil
+// return []string
+func getID(songsPath string) ([]string, error) {
+	songFolders, err := ioutil.ReadDir(songsPath)
+	if err != nil {
+		return nil, err
+	}
+	var idSet []string
+	var splitedID []string
+	for i := 0; i < len(songFolders); i++ {
+		idSet = append(idSet, songFolders[i].Name())
+		splitedID = strings.Split(idSet[i], " ")
+		if _, err := strconv.Atoi(splitedID[0]); err == nil {
+			idSet[i] = splitedID[0]
+		} else {
+			splitedID[0] = "01" + strconv.Itoa(i)
+			idSet[i] = splitedID[0]
+		}
+	}
+	return idSet, nil
 }
 
 func osuSongsPath() (string, error) {
@@ -52,7 +77,7 @@ func osuSongsPath() (string, error) {
 }
 
 //need rewrite makeDirStr func to normal code :C
-//v := `"D:\Games\osu!\osu!.exe",1"`
+//v := `"D:\Games\osu!\osu!.exe" ",1"`
 //v = v[1:len(v)-3]
 func makeDirStr(x string) string {
 	x = strings.Replace(x, "\"", "", 2)
