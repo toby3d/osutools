4ba21cfa8ad6be8e36393d8ec867269718a68671
Compico <compico@mail.ru>
2019-07-16 01:39:52 +0300

    so hard for first program

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # osutools
 in the work
+
+Multifunctional tool for osu
+
+* mp3extrator
+
+  ...the rest is in the plans
+
--- main.go
+++ main.go
@@ -5,59 +5,120 @@ import (
 	"io/ioutil"
 	"log"
 	"path/filepath"
-	"strconv"
 	"strings"
 
 	"golang.org/x/sys/windows/registry"
 )
 
-//SongFolder {id-author-name}
-type SongFolder struct {
-	ID     []string
+//OsuSong {path-artist-title}
+type OsuSong struct {
+	Path   string
 	Artist string
 	Title  string
 }
 
-func main() {
-	songsPath, err := osuSongsPath()
-	if err != nil {
-		fmt.Printf("osu! not installed")
+func test(s []string) {
+	for _, l := range s {
+		fmt.Println(l)
 	}
-	songsFolders, err := getID(songsPath)
+}
+
+func osufiletest(l []string) error {
+	for i := 0; i < len(l); i++ {
+		if filepath.Ext(l[i]) != ".osu" {
+			err := fmt.Errorf(" %s in %v not .osu file", l[i], i)
+			return err
+		}
+	}
+	return nil
+}
+
+func main() {
+	fmt.Println("Searching \"osu!\" folder.")
+	songsPath, err := osuSongsPath() //'%path_to_osu%/Songs'
+	if err != nil {
+		fmt.Println("osu! not installed")
+	}
+	fmt.Println("..done")
+	fmt.Println("Finding beatmaps in folder \"Songs\".")
+	songsFolders, err := lsSongFolder(songsPath) //'%path_to_osu%/Songs/songsFolders[i]'
 	if err != nil {
 		log.Fatal(err)
 	}
-	for _, songFolder := range songsFolders {
-		fmt.Println(songFolder)
+	fmt.Println("..done")
+	fmt.Println("Finding \".osu\" file in folders.")
+	osuFiles, err := osuFileList(songsFolders) //%songspath%/%map%/file.osu
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	fmt.Println("..done")
+	err = osufiletest(osuFiles)
+	if err != nil {
+		fmt.Println(err)
 	}
 }
 
-// return "audio.mp3"
-func findMp3File(songPath string) (string, error) {
-	return "", nil
+// return file.osu list with path
+func osuFileList(sp []string) ([]string, error) {
+	var (
+		listFile, osufilepath, emptyFolders []string
+		err                                 error
+	)
+	for i := 0; i < len(sp); i++ {
+		listFile, err = lsSongFolder(sp[i]) //'%path_to_song%/listfile[i]'
+		if err != nil {
+			return nil, err
+		} else if len(listFile) == 0 {
+			break
+		}
+		p, err := filePosition(listFile) //return position osu file
+		if err != nil {
+			return nil, err
+		} else if p >= 0 {
+			osufilepath = append(osufilepath, listFile[p])
+		} else if p == -1 {
+			emptyFolders = append(emptyFolders, sp[i])
+			//fmt.Printf("In folder %s no have file .osu\n", sp[i])
+		}
+	}
+	if len(emptyFolders) > 0 {
+		fmt.Println("[EROOR] In folder(s):")
+		for _, l := range emptyFolders {
+			fmt.Println("►", l)
+		}
+		fmt.Printf("...no have .osu file. ")
+		fmt.Println("But you can ignore it.")
+	}
+	return osufilepath, nil
 }
 
-// return []string
-func getID(songsPath string) ([]string, error) {
+func filePosition(lf []string) (int, error) {
+	for i := 0; i < len(lf); i++ {
+		if filepath.Ext(lf[i]) == ".osu" {
+			return i, nil
+		}
+	}
+	return -1, nil
+}
+
+// func readosufile(f string) OsuSong {
+// 	return
+// }
+
+//This function take path directory and return directory contents
+func lsSongFolder(songsPath string) ([]string, error) {
 	songFolders, err := ioutil.ReadDir(songsPath)
 	if err != nil {
 		return nil, err
 	}
-	var idSet []string
-	var splitedID []string
+	var strList []string
 	for i := 0; i < len(songFolders); i++ {
-		idSet = append(idSet, songFolders[i].Name())
-		splitedID = strings.Split(idSet[i], " ")
-		if _, err := strconv.Atoi(splitedID[0]); err == nil {
-			idSet[i] = splitedID[0]
-		} else {
-			splitedID[0] = "01" + strconv.Itoa(i)
-			idSet[i] = splitedID[0]
-		}
+		strList = append(strList, filepath.Join(songsPath, songFolders[i].Name()))
 	}
-	return idSet, nil
+	return strList, nil
 }
 
+//Return path '%path_to_osu%/Songs'
 func osuSongsPath() (string, error) {
 	k, err := registry.OpenKey(registry.CLASSES_ROOT, `osu\DefaultIcon`, registry.QUERY_VALUE)
 	if err != nil {
@@ -70,17 +131,9 @@ func osuSongsPath() (string, error) {
 		return "", err
 	}
 
-	path = makeDirStr(path)
+	path = strings.Replace(path, "\"", "", 2) //v := `"D:\Games\osu!\osu!.exe" ",1"`
+	path = filepath.Dir(path)                 //v = v[1:len(v)-3]
 	path = filepath.Join(path, "Songs")
 
 	return path, nil
-}
-
-//need rewrite makeDirStr func to normal code :C
-//v := `"D:\Games\osu!\osu!.exe" ",1"`
-//v = v[1:len(v)-3]
-func makeDirStr(x string) string {
-	x = strings.Replace(x, "\"", "", 2)
-	x = filepath.Dir(x)
-	return x
 }
